4488e6c204c801a19b44d3592fff2bd67c086aa9
= <neagumario21@yahoo.com>
2023-03-05 15:54:18 +0200

    Remove test()

--- server/main.go
+++ server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/binary"
 	"log"
 	"math/rand"
 	"pylon/server"
@@ -9,22 +8,6 @@ import (
 
 	_ "github.com/glebarez/go-sqlite"
 )
-
-func test() {
-	var x uint32 = 354
-
-	bytes := [4]byte{}
-
-	binary.LittleEndian.PutUint32(bytes[0:4], x)
-
-	for _, bt := range bytes {
-		print(bt, " ")
-	}
-	println()
-
-	y := binary.LittleEndian.Uint32(bytes[0:4])
-	println(y)
-}
 
 func main() {
 	log.Println("Server started running")
