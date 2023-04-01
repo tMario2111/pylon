7d3433cc0a057c31e09edd97ceaeb00638f91893
= <neagumario21@yahoo.com>
2023-04-01 12:23:16 +0300

    Create database tables

--- server/server/database.go
+++ server/server/database.go
@@ -19,7 +19,33 @@ func newDb() (*sql.DB, error) {
 		email TEXT NOT NULL UNIQUE,
 		password TEXT NOT NULL,
 		public_key TEXT NOT NULL
-	);`)
+	);
+
+	CREATE TABLE IF NOT EXISTS chats
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+		name TEXT,
+		creator_id INTEGER NOT NULL
+	);
+	
+	CREATE TABLE IF NOT EXISTS messages
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+		user_id INTEGER NOT NULL,
+		chat_id INTEGER NOT NULL,
+		content TEXT NOT NULL,
+		timestamp INTEGER NOT NULL
+	);
+
+	CREATE TABLE IF NOT EXISTS participants
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+		user_id INTEGER NOT NULL,
+		chat_id INTEGER NOT NULL,
+		shared_key TEXT NOT NULL
+	);
+	`)
+
 	if err != nil {
 		println(err.Error())
 		return nil, err
